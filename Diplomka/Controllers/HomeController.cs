3d6e5306e7169bb1b3dd5feade5e815becd0ba2d
Dafank <fentik9814@gmail.com>
2018-04-11 00:51:49 +0300

    add DB add Grammar page

---
+++ Diplomka/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Diplomka.Models;
+
+namespace Diplomka.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Translate()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Translate(string Word)
+        {
+            string s = Translaters.SetData(Word);
+            Object s1 = s;
+            return View(s1);
+        }
+
+        public ActionResult Grammar()
+        {
+            return View();
+        }
+    }
+}
---
+++ Diplomka/Controllers/UserController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Diplomka.Models.User.Login;
+using Diplomka.Models.User.Registration;
+
+namespace Diplomka.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(LoginData login)
+        {
+            DateTime x = new DateTime();
+            if (ModelState.IsValid)
+            {
+                return RedirectToRoute(new { controller = "Home", action = "Index" });
+            }
+            else
+            {
+                return View();
+            }
+        }
+        public ActionResult Registration()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Registration(RegistrationData registration)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToRoute(new { controller = "Home", action = "Index" });
+            }
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
